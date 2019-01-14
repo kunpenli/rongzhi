7de4d8dbd7b49ed1fd6bcefbf7735c1be94e780e
kunpenli <lz13037330@163.com>
2019-01-14 09:36:01 +0800

    通用mapper的位置，之前理解错误，只要不用被mybatis扫描到就行

--- src/main/java/com/utils/BasMapper.java
+++ src/main/java/com/utils/BasMapper.java
@@ -4,6 +4,7 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 public interface BasMapper<T> extends Mapper<T>, MySqlMapper<T> {
-    //TODO
-    //FIXME 特别注意，该接口不能被扫描到，否则会出错
+    // TODO
+    // FIXME 特别注意，该接口不能被MapperScan扫描到，否则会出错！
+    // 开始理解错了，以为是不能被spring容器扫描到，其实是不能被MapperScan扫描到
 }
